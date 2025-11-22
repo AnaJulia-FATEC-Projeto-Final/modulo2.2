a5710c52405bde564c0ba3af2d03e125074c69eb
ajlima12 <ajlimaa12@gmail.com>
2025-11-22 12:45:34 -0300

    repositorios

---
+++ domain/src/main/java/com/anajulia/repositorios/UsuarioRepositorio.java
@@ -0,0 +1,9 @@
+package com.anajulia.repositorios;
+
+import com.anajulia.entidades.Usuario;
+
+public interface UsuarioRepositorio {
+    Usuario salvar(Usuario usuario);
+
+    Usuario procurarPorUsuario(String usuario);
+}
