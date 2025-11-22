428f492b2f731f76ba36a7fb8f43557a2e54f67a
ajlima12 <ajlimaa12@gmail.com>
2025-11-22 13:45:04 -0300

    adicionar comando para criar usuario sistema

--- springboot/src/main/java/com/anajulia/modulo22/Application.java
+++ springboot/src/main/java/com/anajulia/modulo22/Application.java
@@ -1,13 +1,34 @@
 package com.anajulia.modulo22;
 
+import com.anajulia.entidades.Usuario;
+import com.anajulia.repositorios.UsuarioRepositorio;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	private final PasswordEncoder encoder;
+	private final UsuarioRepositorio usuarioRepositorio;
+
+	public Application(PasswordEncoder encoder, UsuarioRepositorio usuarioRepositorio) {
+		this.encoder = encoder;
+		this.usuarioRepositorio = usuarioRepositorio;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario system = new Usuario(
+				"12",
+				"sistema",
+				"123"
+		);
+
+		usuarioRepositorio.salvar(system);
+	}
 }
