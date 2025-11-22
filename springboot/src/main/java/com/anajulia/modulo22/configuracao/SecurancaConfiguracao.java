8069c63e82bee144a85cf378393692ab91169639
ajlima12 <ajlimaa12@gmail.com>
2025-11-22 13:12:28 -0300

    adicionar configuracao

---
+++ springboot/src/main/java/com/anajulia/modulo22/configuracao/SecurancaConfiguracao.java
@@ -0,0 +1,59 @@
+package com.anajulia.modulo22.configuracao;
+
+import com.anajulia.modulo22.seguranca.JwtAutenticacaoFiltroSeguranca;
+import com.anajulia.modulo22.seguranca.JwtSeguranca;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurancaConfiguracao {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            HttpSecurity httpSecurity,
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailsService
+    ) throws Exception {
+        AuthenticationManagerBuilder builder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+
+        return builder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            JwtSeguranca jwtSecurity,
+            HttpSecurity httpSecurity,
+            UserDetailsService userDetails
+    ) throws Exception {
+        JwtAutenticacaoFiltroSeguranca jwtFilter = new JwtAutenticacaoFiltroSeguranca(jwtSecurity, userDetails);
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/anajulia/modulo2.2/login/**"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+}
